752def28e35dcdd4a0a6b233f0319e43e4d53d7f
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-22 09:17:07 -0300

    add title component and v-on:dblclick

---
+++ src/components/shared/panel/Panel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="c_panel">
+    <h2 class="panel__title" v-on:dblclick="visible = !visible">{{ titulo }}</h2>
+    <div class="panel__content" v-show="visible">
+      <slot name="panel-content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['titulo'],
+
+  data() {
+    return {
+      visible: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .c_panel {
+    width: 250px;
+  }
+
+  .panel__title {
+    text-align: center;
+    cursor: pointer;
+    background: blue;
+    color: white;
+    padding: 5px;
+  }
+
+</style>
---
+++ src/components/shared/title/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="title">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+
+  props: ['titulo']
+}
+</script>
+
+<style scoped>
+  .title {
+    text-align: center;
+  }
+
+</style>
