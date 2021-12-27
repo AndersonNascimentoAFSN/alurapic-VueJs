5a96022be57e3b8e1d532d092db1b0b7b759badb
Anderson F. S. do Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-27 09:15:14 -0300

    add page Register

--- src/components/register/Register.vue
+++ src/components/register/Register.vue
@@ -1,22 +1,73 @@
 <template>
-  <!-- <Title v-text="title" /> -->
-  <Title v-bind:title="title" />
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+
+    <form>
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off">
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off">
+        <ImageResponsive/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <Button label="GRAVAR" type="submit"/>
+        <router-link to="/"><Button label="VOLTAR" type="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
 
 <script>
-  import Title from '../shared/title/Title.vue';
 
-  export default {
-    components: {
-      Title,
-    },
-    data() {
-      return {
-        title: 'Cadastro',
-      }
-    }
+import ImageResponsive from '../shared/image-responsive/ImageResponsive.vue'
+import Button from '../shared/button/Button.vue';
+
+export default {
+
+  components: {
+
+    ImageResponsive, 
+    Button,
   }
-</script>
+}
 
+</script>
 <style scoped>
+
+  .centralizado {
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+  .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
 </style>
