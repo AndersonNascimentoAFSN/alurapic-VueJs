3229c9d0c961027e441afe7584bad0ae3a7cbbfc
Anderson F. S. do Nascimento <anderson.nascimento@meta.com.br>
2022-01-11 09:28:38 -0300

    Carregamento Lazy

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import Home from './components/home/Home.vue';
-import Register from './components/register/Register.vue';
+const Register = () => System.import('./components/register/Register.vue');
 
 export const routes = [
   { path: '/', name: "Home", component: Home, title: 'Home', menu: true },
